d172c3a6ba414c8c0a2a32101f67150635877e0c
qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
2021-12-27 18:35:30 +0800

    libhelix-mp3: add Makefile
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>
    Signed-off-by: qiaohaijiao1 <qiaohaijiao1@xiaomi.com>

--- real/assembly.h
+++ real/assembly.h
@@ -267,6 +267,155 @@ static __inline int CLZ(int x)
 	return numZeros;
 }
 
+#elif defined(__GNUC__) && defined(__i386__)
+
+
+typedef long long Word64;
+
+static __inline__ int MULSHIFT32(int x, int y)
+{
+    int z;
+
+    z = (Word64)x * (Word64)y >> 32;
+
+    return z;
+}
+
+static __inline short CLIPTOSHORT(int x)
+{
+	int sign;
+
+	/* clip to [-32768, 32767] */
+	sign = x >> 31;
+	if (sign != (x >> 15))
+		x = sign ^ ((1 << 15) - 1);
+
+	return (short)x;
+}
+
+static __inline int FASTABS(int x)
+{
+	int sign;
+
+	sign = x >> (sizeof(int) * 8 - 1);
+	x ^= sign;
+	x -= sign;
+
+	return x;
+}
+
+static __inline int CLZ(int x)
+{
+	int numZeros;
+
+	if (!x)
+		return 32;
+
+	/* count leading zeros with binary search (function should be 17 ARM instructions total) */
+	numZeros = 1;
+	if (!((unsigned int)x >> 16))	{ numZeros += 16; x <<= 16; }
+	if (!((unsigned int)x >> 24))	{ numZeros +=  8; x <<=  8; }
+	if (!((unsigned int)x >> 28))	{ numZeros +=  4; x <<=  4; }
+	if (!((unsigned int)x >> 30))	{ numZeros +=  2; x <<=  2; }
+
+	numZeros -= ((unsigned int)x >> 31);
+
+	return numZeros;
+}
+
+typedef union _U64 {
+	Word64 w64;
+	struct {
+		/* x86 = little endian */
+		unsigned int lo32;
+		signed int   hi32;
+	} r;
+} U64;
+
+static __inline Word64 MADD64(Word64 sum64, int x, int y)
+{
+	sum64 += (Word64)x * (Word64)y;
+
+	return sum64;
+}
+
+static __inline Word64 SAR64(Word64 x, int n) {
+	return x >>= n;
+}
+
+#elif __xtensa__
+typedef long long Word64;
+
+static __inline__ int MULSHIFT32(int x, int y)
+{
+    int z;
+
+    z = (Word64)x * (Word64)y >> 32;
+
+    return z;
+}
+
+static __inline short CLIPTOSHORT(int x)
+{
+	int sign;
+
+	/* clip to [-32768, 32767] */
+	sign = x >> 31;
+	if (sign != (x >> 15))
+		x = sign ^ ((1 << 15) - 1);
+
+    return (short)x;
+}
+
+static __inline int FASTABS(int x)
+{
+	int sign;
+
+	sign = x >> (sizeof(int) * 8 - 1);
+	x ^= sign;
+	x -= sign;
+
+	return x;
+}
+
+static __inline int CLZ(int x)
+{
+	int numZeros;
+
+	if (!x)
+		return 32;
+
+	/* count leading zeros with binary search (function should be 17 ARM instructions total) */
+	numZeros = 1;
+	if (!((unsigned int)x >> 16))	{ numZeros += 16; x <<= 16; }
+	if (!((unsigned int)x >> 24))	{ numZeros +=  8; x <<=  8; }
+	if (!((unsigned int)x >> 28))	{ numZeros +=  4; x <<=  4; }
+	if (!((unsigned int)x >> 30))	{ numZeros +=  2; x <<=  2; }
+
+	numZeros -= ((unsigned int)x >> 31);
+
+	return numZeros;
+}
+
+typedef union _U64 {
+	Word64 w64;
+	struct {
+		unsigned int lo32;
+		signed int   hi32;
+	} r;
+} U64;
+
+static __inline Word64 MADD64(Word64 sum64, int x, int y)
+{
+	sum64 += (Word64)x * (Word64)y;
+
+	return sum64;
+}
+
+static __inline Word64 SAR64(Word64 x, int n) {
+	return x >>= n;
+}
+
 #elif defined(__GNUC__) && defined(ARM)
 
 #if defined(ARM7DI)
